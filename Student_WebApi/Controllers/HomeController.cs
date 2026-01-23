5bb175227e635739132c8075b1fad51805977766
dshettyy <dishanthshettyy@gmail.com>
2026-01-23 11:12:20 +0530

    Initial Commit

---
+++ Student_WebApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Student_WebApi.Models;
+
+namespace Student_WebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppSettings _settings;
+        public HomeController(IOptions<AppSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+        public IActionResult Index()
+        {
+            return Content(
+                $"Name of the Application : {_settings.ApplicationName} \n" + 
+                $"Support : {_settings.SupportEmail}");
+        }
+    }
+}
---
+++ Student_WebApi/Models/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Student_WebApi.Models
+{
+    public class AppSettings
+    {
+        public string ApplicationName { get; set; }
+        public string SupportEmail { get; set; }
+    }
+}
